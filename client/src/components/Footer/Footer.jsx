5434129c608b5ea0f14977fd7b61200e3f2118a8
danielhyr <danhyunrha@gmail.com>
2021-06-28 19:14:44 -0400

    created a responsive footer component

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.scss'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>&copy; InStock Inc. All Rights Reserved.</p>
+        </div>
+    )
+}
+
+export default Footer
