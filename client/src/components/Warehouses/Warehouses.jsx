412b21eb9aec12664fc33ef9501167fa9126eae9
dkirali <kiralidoruk@gmail.com>
2021-06-29 14:36:45 -0400

    halfway through completing warehouse list component

--- client/src/components/Warehouses/Warehouses.jsx
+++ client/src/components/Warehouses/Warehouses.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './Warehouses.scss'
 import search from "../../assets/icons/search-24px.svg"
+import Trash from "../../assets/icon/delete_outline-24px.svg"
+import Edit from "../../assets/icon/edit-24px.svg"
 
 const Warehouses = () => {
     return(
@@ -9,34 +11,40 @@ const Warehouses = () => {
                 <h1 className="warehouse__title">Warehouses</h1>
                 <form className="warehouse__form">
                     <label className="warehouse__form-label" htmlFor="text"></label>
-                    <input className="warehouse__form-search" placeholder="Search" name="text"/>
+                    <input className="warehouse__form-search" placeholder="Search..." name="text"/>
                         <img className="warehouse__form-search-icon" src={search} alt="search-icon"/>
                 </form>
-                <button className="warehouse__button" type="submit"> +Add New Warehouse </button>
+                <button className="warehouse__button" type="submit"><h3 className="warehouse__button-text">+Add New Warehouse</h3> </button>
             </div>
             <div className="warehouse__information">
-                <div className="warehouse__information-top">
-                    <div className="warehouse__information-location">
-                        <h4 className="warehouse__subheader">WAREHOUSE</h4>
-                        <h3 className="warehouse__location">Manhattan</h3>
+                <div className="warehouse__information-data">
+                    <div className="warehouse__information-top">
+                        <div className="warehouse__information-location">
+                            <h4 className="warehouse__subheader">WAREHOUSE</h4>
+                            <h3 className="warehouse__location">Manhattan </h3>
+                        </div>
+                        <div className="warehouse__information-address">
+                            <h4 className="warehouse__subheader">ADDRESS</h4>
+                            <p className="warehouse__address-details">503 Broadway, New York, USA</p>
+                        </div>
                     </div>
-                    <div className="warehouse__information-contact">
-                        <h4 className="warehouse__subheader">CONTACT NAME</h4>
-                        <p className="warehouse__contact-name">Parmin Aujula</p>
+                    <div className="warehouse__information-bottom">
+                        <div className="warehouse__information-contact">
+                            <h4 className="warehouse__subheader">CONTACT NAME</h4>
+                            <p className="warehouse__contact-name">Parmin Aujula</p>
+                        </div>
+                        <div className="warehouse__information-contact-information">
+                            <h4 className="warehouse__subheader">CONTACT INFORMATION</h4>
+                            <p className="warehouse__contact-number">+1(629)555-0129</p>
+                            <p className="warehouse__contact-email">paujla@instock.com</p>
+                        </div>
                     </div>
                 </div>
-                <div className="warehouse__information-bottom">
-                    <div className="warehouse__information-address">
-                        <h4 className="warehouse__subheader">ADDRESS</h4>
-                        <p className="warehouse__address-details">503 Broadway, New York, USA</p>
-                    </div>
-                    <div className="warehouse__information-contact-information">
-                        <h4 className="warehouse__subheader">CONTACT INFORMATION</h4>
-                        <p className="warehouse__contact-number">+1(629)555-0129</p>
-                        <p className="warehouse__contact-email">paujla@instock.com</p>
-                    </div>
+                <div className="warehouse__actions">
+                    <img claasName ="warehouse__action-trash" src={Trash} alt="trashcan"/>
+                    <img claasName ="warehouse__action-edit"src={Edit} alt="trashcan"/>
                 </div>
-            </div>
+            </div> 
         </section>  
     )
 }
