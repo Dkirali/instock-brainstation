93dd5c59e20798e95d669d3975c317faecd51fb9
dkirali <kiralidoruk@gmail.com>
2021-06-29 00:02:36 -0400

    chaned the header

--- client/src/components/HeroHeader/HeroHeader.jsx
+++ client/src/components/HeroHeader/HeroHeader.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Logo from '../../assets/logo/logo1.png'
+import './HeroHeader.scss'
 
 const HeroHeader = () => {
     return(
-        <div>
-            <nav>
-                <ul>
-                    <li>logo</li>
-                    <li>Warehouses</li>
-                    <li>Inventory</li>
-                </ul>
-            </nav>
-        </div>
+        <nav className="header">
+            <img src={Logo} alt="instock logo two arrows"/>
+            <div className="header-container">
+                <Link className="header-container__link">Warehouses</Link>
+                <Link className="header-container__link">Inventory</Link>
+            </div>
+        </nav >
     )
 }
 
