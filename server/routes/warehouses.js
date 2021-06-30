21b85c27339948c7b71d7a4f7ac93575e94d3037
brsilva181 <brsilva181@gmail.com>
2021-06-29 23:03:49 -0400

    Created API to get inverntories for a given warehouse by ID

--- server/routes/warehouses.js
+++ server/routes/warehouses.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const warehouses = require("../data/warehouses.json");
+const inventory = require("../data/inventories.json");
 
 const getWarehouse = (id) => {
     const foundWarehouse = warehouses.find((warehouse) => {
@@ -13,6 +14,15 @@ router.get("/:id", (req, res) => {
     let { id } = req.params;
     const warehouseFound = getWarehouse(id);
     res.status(200).json(warehouseFound);
+});
+
+router.get("/:id/inventory", (req, res) => {
+    let { id } = req.params;
+    const warehouseFound = getWarehouse(id);
+    const warehouseInventory = inventory.filter((item) => {
+        return item.warehouseID === warehouseFound?.id;
+    });
+    res.status(200).json(warehouseInventory);
 });
 
 module.exports = router;
