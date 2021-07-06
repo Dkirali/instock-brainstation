f8d88264d654a0c4a61b8a78eb9913eee3831372
dkirali <kiralidoruk@gmail.com>
2021-07-05 22:50:59 -0400

    fixes

--- client/src/components/AddItem/AddItem.jsx
+++ client/src/components/AddItem/AddItem.jsx
@@ -2,23 +2,27 @@ import ItemForm from '../ItemForm/ItemForm'
 import React, { Component } from 'react'
 import axios from 'axios'
 import { API_URL } from "../../utils/utils";
-
 export default class AddItem extends Component {
-
 submitHandler = (formData) => {
     axios.post(`${API_URL}/inventory/add`, formData)
+    .then(() => {
+        this.props.history.push('/inventory')})
+    .catch((err) => {
+        console.log(err)
+    })
     }
-
-
+clickHandler = () => {
+    this.props.history.push('/inventory')
+}
     render() {
         return (
-            
-            
             <ItemForm
             title="Add New Inventory Item"
             handleSubmit={this.submitHandler} 
-            button="+ Add Item" />
-
+            button="+ Add Item" 
+            handleClick={this.clickHandler}
+            route="/inventory"
+            />
         )
     }
 }
